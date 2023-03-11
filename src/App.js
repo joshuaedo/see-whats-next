7a3f1bdf117709489626de265ebd7bb8a62c09e1
Joshua <joshua.edo01@gmail.com>
2023-03-11 22:48:28 +0100

    2nd deploy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import "./App.css";
-import HomeScreen from "./screens/HomeScreen";
-import LoginScreen from "./screens/LoginScreen";
-import { Route, Routes } from "react-router-dom";
+import './App.css';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const user = null;
+  const user = {
+    name: 'Joshua',
+    email: 'joshua.edo01@gmail.com',
+  };
   return (
-    <div className="app">
+    <div className='app'>
       <Routes>
         {user ? (
           <>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/login" element={<LoginScreen />} />
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/login' element={<LoginScreen />} />
           </>
         ) : (
-          <Route path="/" element={<LoginScreen />} />
+          <Route path='/' element={<LoginScreen />} />
         )}
       </Routes>
     </div>
