08f894d1c368aa0030baa232dd437d9ea062243a
Joshua Edo <joshua.edo01@gmail.com>
2023-03-14 12:30:56 +0000

    add carousel buttons

--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./Row.css";
 import axios from "./axios";
 import Youtube from "react-youtube";
@@ -10,6 +10,8 @@ export default function Row({ title, fetchUrl }) {
   const API_KEY = "f025efcd2e89d572e5a3ac07a0ce13d3";
   const [clickedMovieId, setClickedMovieId] = useState(null);
   const [videoId, setVideoId] = useState(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const rowRef = useRef(null);
 
   async function fetchVideoId(movieId) {
     const response = await axios.get(
@@ -36,16 +38,103 @@ export default function Row({ title, fetchUrl }) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
 
+  function handleLeftClick() {
+    const container = rowRef.current;
+    if (container) {
+      const scrollStep = -200;
+      const start = container.scrollLeft;
+      let currentTime = 0;
+
+      function animateScroll() {
+        currentTime += 10;
+        const newPosition = Math.easeInOutQuad(
+          currentTime,
+          start,
+          scrollStep,
+          500
+        );
+        container.scrollLeft = newPosition;
+        setScrollPosition(container.scrollLeft);
+
+        if (currentTime < 500) {
+          requestAnimationFrame(animateScroll);
+        }
+      }
+
+      requestAnimationFrame(animateScroll);
+    }
+  }
+
+  function handleRightClick() {
+    const container = rowRef.current;
+    if (container) {
+      const scrollStep = 200;
+      const start = container.scrollLeft;
+      let currentTime = 0;
+
+      function animateScroll() {
+        currentTime += 10;
+        const newPosition = Math.easeInOutQuad(
+          currentTime,
+          start,
+          scrollStep,
+          500
+        );
+        container.scrollLeft = newPosition;
+        setScrollPosition(container.scrollLeft);
+
+        if (currentTime < 500) {
+          requestAnimationFrame(animateScroll);
+        }
+      }
+
+      requestAnimationFrame(animateScroll);
+    }
+  }
+
+  // Easing function for smooth scrolling
+  Math.easeInOutQuad = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  };
+
+  function handleMouseEnter() {
+    setHovering(true);
+  }
+
+  function handleMouseLeave() {
+    setHovering(false);
+  }
+
+  const [hovering, setHovering] = useState(false);
+
   return (
-    <div className="row">
+    <div
+      className="row"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <h3>{title}</h3>
-      <div className="row__posters">
+      <div className="row__posters" ref={rowRef}>
+        {hovering && (
+          <>
+            <button onClick={handleLeftClick} className="left">
+              &gt;
+            </button>
+            <button onClick={handleRightClick} className="right">
+              &lt;
+            </button>
+          </>
+        )}
+
         {movies.map(
           (movie) =>
             movie.backdrop_path && (
               <>
-                {/* I want the div to render immediately and disapper as soon as the immage has loaded */}
                 <div
+                  key={movie.id}
                   className="skeletonRow__poster"
                   style={{ display: loaded ? "none" : "block" }}
                 />
