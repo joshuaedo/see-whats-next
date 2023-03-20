afbdd465b748fc91ed3002b5dd22fd4705cbba43
Joshua <joshua.edo01@gmail.com>
2023-03-20 08:37:55 +0100

    edit loginScreen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,33 +1,29 @@
-import "./LoginScreen.css";
-import LoginNav from "../LoginNav";
-import { useState } from "react";
-import SignInScreen from "./SignInScreen";
+import './LoginScreen.css';
+import LoginNav from '../LoginNav';
+import { useState } from 'react';
+import SignInScreen from './SignInScreen';
 
 export default function LoginScreen() {
   const [signIn, setSignIn] = useState(false);
 
   return (
     <>
-      <div className="loginScreen">
+      <div className='loginScreen'>
         <LoginNav
           onClick={() => {
             setSignIn(true);
           }}
         />
-        <div className="loginScreen__gradient" />
+        <div className='loginScreen__gradient' />
 
         {signIn ? (
           <SignInScreen />
         ) : (
-          <div className="loginScreen__body">
+          <div className='loginScreen__body'>
             <h1>Unlimited films, Tv Programmes and more</h1>
             <h2>Watch any where, cancel at any time</h2>
-            <h3>
-              Ready to watch? Enter your email to create or restart your
-              membership
-            </h3>
 
-            <div className="loginScreen__input">
+            <div className='loginScreen__input'>
               <form>
                 <button
                   onClick={() => {
